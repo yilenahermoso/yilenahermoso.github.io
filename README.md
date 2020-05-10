5a74bafa5b1f79d8628ff40504d4f9614450dbee
yilenahermoso <63805800+yilenahermoso@users.noreply.github.com>
2020-05-10 10:52:52 +0200

    Yilena

--- README.md
+++
@@ -1,4 +0,0 @@
-# Pagina web de pruebas
-
-Este es un repositorio de pruebas para clase
-est
---
+++ js/js.js
@@ -0,0 +1,4 @@
+document.getElementById("header").style.height = window.innerHeight+"px";
+document.getElementById("textheader").style.marginTop = window.innerHeight/2.5+"px";
+
+
