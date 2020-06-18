5898f2c143faa8b478282d865d2c3397bba5a16d
yilenahermoso <63805800+yilenahermoso@users.noreply.github.com>
2020-06-18 20:05:12 +0200

    yile_web

--- js/js.js
+++ js/js.js
@@ -2,3 +2,4 @@ document.getElementById("header").style.height = window.innerHeight+"px";
 document.getElementById("textheader").style.marginTop = window.innerHeight/2.5+"px";
 
 
+
---
+++ js/municipios.js
@@ -0,0 +1,10 @@
+var municipios = {
+    "type": "FeatureCollection",
+    "name": "municipios",
+    "crs": { "type": "name", "properties":
+    { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "nuts_id": "ES241" },
+    "geometry": { "type": "MultiPolygon", "coordinates":
+    [[ -0.750774, 42.9240265 ], [ -0.7441135, 42.9191705 ],
+    ]}}]};
+    
