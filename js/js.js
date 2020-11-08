ca109e6669b067dabdcfba918d052e03914ac55a
yilenahermoso <63805800+yilenahermoso@users.noreply.github.com>
2020-11-08 01:49:33 +0100

    YilenaHermoso

--- js/js.js
+++ js/js.js
@@ -1,5 +1,16 @@
-document.getElementById("header").style.height = window.innerHeight+"px"; //elemento con id HEADER se le ha dado una altura igual a la que mide la ventana del navegador 
-document.getElementById("textheader").style.marginTop = window.innerHeight/2.5+"px"; //elemento con id TEXTHEADER igual a la ventana del navegador/2,5, para darle al elemento el estilo de altura. 
+//document.getElementById("header").style.height = window.innerHeight+"px"; //elemento con id HEADER se le ha dado una altura igual a la que mide la ventana del navegador 
+//document.getElementById("textheader").style.marginTop = window.innerHeight/2.5+"px"; //elemento con id TEXTHEADER igual a la ventana del navegador/2,5, para darle al elemento el estilo de altura. 
 
 
+//Evendo de scroll
+/*window.addEventListener('scroll', () => {
+    const header = document.querySelector('#header');
+    const textHeader = document.querySelector('#textheader');
 
+    header.classList.remove('pantallaCompleta');
+    header.classList.add('reducirScroll');
+    textHeader.classList.remove('textheader');
+    textHeader.classList.add('reducirTextheader');
+    console.log('hola');
+});
+*/
