2167fcbd93f4155b17eb3518e2b65d7ae5d32cd1
yilenahermoso <63805800+yilenahermoso@users.noreply.github.com>
2020-11-11 19:07:10 +0100

    new

--- js/js.js
+++ js/js.js
@@ -1,16 +1,40 @@
 //document.getElementById("header").style.height = window.innerHeight+"px"; //elemento con id HEADER se le ha dado una altura igual a la que mide la ventana del navegador 
 //document.getElementById("textheader").style.marginTop = window.innerHeight/2.5+"px"; //elemento con id TEXTHEADER igual a la ventana del navegador/2,5, para darle al elemento el estilo de altura. 
+/*
+console.log('inicio');
+const proyecto = document.querySelector('.proyectos');
 
+cargarEventListeners();
 
-//Evendo de scroll
-/*window.addEventListener('scroll', () => {
-    const header = document.querySelector('#header');
-    const textHeader = document.querySelector('#textheader');
+function cargarEventListeners() {
+    //cuando agregas un curso presionando "agregar al carrito"
+    proyecto.addEventListener('click', desplegar);
+}
 
-    header.classList.remove('pantallaCompleta');
-    header.classList.add('reducirScroll');
-    textHeader.classList.remove('textheader');
-    textHeader.classList.add('reducirTextheader');
-    console.log('hola');
-});
+//Funciones
+function desplegar(e) {
+    const conten = document.querySelector('.contenido');
+    if (e.target.classList.contains('clamp')) {
+        conten.classList.remove('clamp');
+    }else {
+        conten.classList.add('clamp');
+    }
+}
 */
+
+
+
+
+// const im2 = document.querySelector('.im2');
+// console.log(im2);
+
+// im2.addEventListener('mouseenter', (e) => {
+//     console.log('Entrar al nav');
+//     im2.setAttribute('src', './img/grey.jpg');
+//     e.stopPropagation(1);
+// });
+// im2.addEventListener('mouseout', (e) => {
+//     console.log('Saliendo al nav');
+//     im2.setAttribute('src', './img/Efimeros_estudio_sosa.jpg');
+//     e.stopPropagation(1);
+// });
